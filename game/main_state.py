89574fdcbaadab61b99df31432c5cd6f902959a5
jjw610 <wjj610@naver.com>
2017-11-14 17:40:55 +0900

    충돌체크 상태변화 오브젝트 추가

--- game/main_state.py
+++ game/main_state.py
@@ -9,55 +9,47 @@ name = "MainState"
 
 class Human:
     RIGHT_RUN, DOWN_RUN, LEFT_RUN, UP_RUN, RIGHT_STAND, DOWN_STAND, LEFT_STAND, UP_STAND = 0, 1, 2, 3, 4, 5, 6, 7
-    def handle_left_run(self):
-        if self.x > 0:
-            self.x -= 6
-    def handle_left_stand(self):
-        pass
-    def handle_right_run(self):
-        if self.x < 800:
-            self.x += 6
-    def handle_right_stand(self):
-        pass
-    def handle_up_run(self):
-        if self.y < 800:
-            self.y += 6
-    def handle_up_stand(self):
-        pass
-    def handle_down_run(self):
-        if self.y > 0:
-            self.y -= 6
-    def handle_down_stand(self):
-        pass
+    collideobject = 0
+    def get_bb(self):
+        return self.x - 10, self.y - 35, self.x + 12, self.y + 20
 
-    handle_state = {
-        RIGHT_RUN: handle_right_run,
-        DOWN_RUN: handle_down_run,
-        LEFT_RUN: handle_left_run,
-        UP_RUN: handle_up_run,
-        RIGHT_STAND: handle_right_stand,
-        DOWN_STAND: handle_down_stand,
-        LEFT_STAND: handle_left_stand,
-        UP_STAND: handle_left_stand
-    }
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
 
-    def __init__(self):
+    def __init__(self, x, y ):
         self.state = 4
-        self.x, self.y = 400, 300
+        self.x, self.y = x, y
         self.frame = 0
         self.image = load_image('image\character\human.png')
         self.dir = 4
 
     def update(self):
         self.frame = (self.frame + 1) % 9
-        self.handle_state[self.state](self)
+        if self.collideobject == 0:
+            if self.state in (self.RIGHT_RUN,):
+                self.x = min(764, self.x + 4)
+            elif self.state in (self.LEFT_RUN,):
+                self.x = max(32, self.x - 4)
+            if self.state in (self.UP_RUN, ):
+                self.y = min(556, self.y + 4)
+            elif self.state in (self.DOWN_RUN, ):
+                self.y = max(40, self.y - 4)
+        else:
+            if self.state in (self.RIGHT_RUN,):
+                self.x = self.x
+            elif self.state in (self.LEFT_RUN,):
+                self.x = self.x
+            if self.state in (self.UP_RUN, ):
+                self.y = self.y
+            elif self.state in (self.DOWN_RUN, ):
+                self.y = self.y
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                if human.state in (human.DOWN_RUN, human.LEFT_RUN, human.UP_RUN, human.DOWN_STAND, human.UP_STAND, human.RIGHT_STAND, human.LEFT_STAND):
-                    human.state = 0
+                if self.state in (self.DOWN_RUN, self.LEFT_RUN, self.UP_RUN, self.DOWN_STAND, self.UP_STAND, self.RIGHT_STAND, self.LEFT_STAND):
+                    self.state = 0
             elif event.key == SDLK_DOWN:
-                if human.state in (human.LEFT_RUN, human.UP_RUN, human.RIGHT_RUN, human.DOWN_STAND, human.UP_STAND, human.RIGHT_STAND, human.LEFT_STAND):
+                if self.state in (self.LEFT_RUN, self.UP_RUN, self.RIGHT_RUN,self.DOWN_STAND, self.UP_STAND, self.RIGHT_STAND, self.LEFT_STAND):
                     human.state = 1
             elif event.key == SDLK_LEFT:
                 if human.state in (human.DOWN_RUN, human.UP_RUN, human.RIGHT_RUN, human.DOWN_STAND, human.UP_STAND,human.RIGHT_STAND, human.LEFT_STAND):
@@ -108,6 +100,45 @@ class Human:
         else:
             self.image.clip_draw(self.frame * 0, ((self.state-4) * 64) - 1, 64, 64, self.x, self.y)
 
+#----------------------------------------------------------------------------------------------------------------
+
+class skeleton:
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND, = 0, 1, 2, 3
+    UP_RUN, UP_STAND, DOWN_RUN, DOWN_STAND = 4,5,6,7
+
+
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.state = self.RIGHT_STAND
+        if skeleton.image == None:
+            skeleton.image = load_image('image\character\skeleton.png')
+
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        self.frame = (self.frame +1)% 9
+        if self.state in (self.RIGHT_RUN, ):
+            self.x = min(800, self.x +1)
+        elif self.state in (self.LEFT_RUN, ):
+            self.x = max(0, self.x -1)
+        if self.state in (self.RIGHT_UP, self.LEFT_UP, self.UP_RUN_RIGHT, self.UP_RUN_LEFT):
+            self.y = min(600, self.y +1)
+        elif self.state in (self.RIGHT_DOWN, self.LEFT_DOWN, self.DOWN_RUN_RIGHT, self.DOWN_RUN_LEFT):
+            self.y = max(0, self.y -1)
+        pass
+
+    def draw(self):
+        if self.state in (self.LEFT_DOWN, self.LEFT_UP, self.UP_RUN_LEFT,self.DOWN_RUN_LEFT):
+            self.image.clip_draw(self.frame * 100, 0 * 100, 100, 100, self.x, self.y)
+        elif self.state in (self.RIGHT_DOWN, self.RIGHT_UP,self.UP_RUN_RIGHT,self.DOWN_RUN_RIGHT):
+            self.image.clip_draw(self.frame * 100, 1 * 100, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+
 class BackGround:
     def __init__(self):
         self.image = load_image('image\ground\state1-1.png')
@@ -115,31 +146,68 @@ class BackGround:
     def draw(self):
         self.image.draw(400, 300)
 class Object():
-    def __init__(self):
-        self.obn = 0
-        self.x, self.y = 400, 300
+
+
+
+    def __init__(self, type, x, y):
+        self.obn = type
+        self.x, self.y = x, y
         if self.obn == 0:
             self.image = load_image('image\object\oox1-1.png')
+            self.xx = 18
+            self.yy = 22
         elif self.obn == 1:
             self.image = load_image('image\object\oox2-1.png')
+            self.xx = 31
+            self.yy = 37
         elif self.obn == 2:
             self.image = load_image('image\object\itable1-1.png')
+            self.xx = 42
+            self.yy = 30
         elif self.obn == 3:
+            self.image = load_image('image\object\itable2-1.png')
+            self.xx = 42
+            self.yy = 30
+        elif self.obn == 4:
             self.image = load_image('image\object\itable3-1.png')
+            self.xx = 42
+            self.yy = 30
+
+    def get_bb(self):
+        return self.x - self.xx, self.y - self.yy, self.x + self.xx, self.y + self.yy
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
 
 
     def draw(self):
-        self.image.draw(400, 300)
+        self.image.draw(self.x,self.y)
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+    if left_a > right_b:
+        return False
+    elif right_a < left_b:
+        return False
+    elif top_a < bottom_b:
+        return False
+    elif bottom_a > top_b:
+        return False
 
 
+
+    return True
 
 def enter():
+
     global human
     global background
     global box
-    human = Human()
+    human = Human(50, 500)
     background = BackGround()
-    box = Object()
+    box = Object(3, 400,300 )
+
 def exit():
     global human
     global background
@@ -158,6 +226,7 @@ def resume():
 
 def handle_events():
     global human
+    global box
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -167,16 +236,32 @@ def handle_events():
         else:
             human.handle_event(event)
             pass
+    if collide(human, box):
+        human.collideobject = 1
+    else:
+        human.collideobject = 0
+
 
 def update():
+
     human.update()
-    delay(0.05)
+    delay(0.03)
+
+
 def draw():
     clear_canvas()
     background.draw()
     box.draw()
     human.draw()
+
+    human.draw_bb()
+    box.draw_bb()
+
+
     update_canvas()
+
+
+
 
 
 
