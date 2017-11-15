bd64e3bc4d916ff3269b691816ac8f6f7f481d7f
jjw610 <wjj610@naver.com>
2017-11-15 15:18:34 +0900

    해골추가, 랜덤움직임 구현

--- game/main_state.py
+++ game/main_state.py
@@ -111,44 +111,70 @@ class Human:
         else:
             self.image.clip_draw(self.frame * 0, ((self.state-4) * 64) - 1, 64, 64, self.x, self.y)
 
-#----------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
+class Skeleton:
+    RIGHT_RUN, DOWN_RUN, LEFT_RUN, UP_RUN, RIGHT_STAND, DOWN_STAND, LEFT_STAND, UP_STAND = 0, 1, 2, 3, 4, 5, 6, 7
+    collideobject = 0
+    def get_bb(self):
+        return self.x - 10, self.y - 35, self.x + 12, self.y-25
 
-class skeleton:
-    image = None
+    def get_xy(self):
+        return self.x, self.y;
 
-    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND, = 0, 1, 2, 3
-    UP_RUN, UP_STAND, DOWN_RUN, DOWN_STAND = 4,5,6,7
+    def get_st(self):
+        return self.state
 
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
 
-    def __init__(self):
-        self.x, self.y = random.randint(100, 700), 90
-        self.frame = random.randint(0, 7)
-        self.state = self.RIGHT_STAND
-        if skeleton.image == None:
-            skeleton.image = load_image('image\character\skeleton.png')
+    def __init__(self, x, y ):
+        self.nowst = 0
+        self.runtime = 0
+        self.state = 4
+        self.x, self.y = x, y
+        self.frame = 0
+        self.image = load_image('image\character\skeleton.png')
 
-    def handle_event(self, event):
-        pass
 
     def update(self):
-        self.frame = (self.frame +1)% 9
-        if self.state in (self.RIGHT_RUN, ):
-            self.x = min(800, self.x +1)
-        elif self.state in (self.LEFT_RUN, ):
-            self.x = max(0, self.x -1)
-        if self.state in (self.RIGHT_UP, self.LEFT_UP, self.UP_RUN_RIGHT, self.UP_RUN_LEFT):
-            self.y = min(600, self.y +1)
-        elif self.state in (self.RIGHT_DOWN, self.LEFT_DOWN, self.DOWN_RUN_RIGHT, self.DOWN_RUN_LEFT):
-            self.y = max(0, self.y -1)
-        pass
 
-    def draw(self):
-        if self.state in (self.LEFT_DOWN, self.LEFT_UP, self.UP_RUN_LEFT,self.DOWN_RUN_LEFT):
-            self.image.clip_draw(self.frame * 100, 0 * 100, 100, 100, self.x, self.y)
-        elif self.state in (self.RIGHT_DOWN, self.RIGHT_UP,self.UP_RUN_RIGHT,self.DOWN_RUN_RIGHT):
-            self.image.clip_draw(self.frame * 100, 1 * 100, 100, 100, self.x, self.y)
+        self.frame = (self.frame + 1) % 9
+        if self.collideobject == 0:
+            if self.state in (self.RIGHT_RUN,):
+                self.x = min(764, self.x + 4)
+            elif self.state in (self.LEFT_RUN,):
+                self.x = max(32, self.x - 4)
+            if self.state in (self.UP_RUN, ):
+                self.y = min(556, self.y + 4)
+            elif self.state in (self.DOWN_RUN, ):
+                self.y = max(40, self.y - 4)
         else:
-            self.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+            if self.state in (self.RIGHT_RUN,):
+                if self.collideobject != 1:
+                    self.x = min(764, self.x + 4)
+            elif self.state in (self.LEFT_RUN,):
+                if self.collideobject != 3:
+                    self.x = max(32, self.x - 4)
+            if self.state in (self.UP_RUN, ):
+                if self.collideobject != 4:
+                    self.y = min(556, self.y + 4)
+            elif self.state in (self.DOWN_RUN, ):
+                if self.collideobject != 2:
+                    self.y = max(40, self.y - 4)
+    def handle_event(self):
+        self.runtime = self.runtime + 1
+        if(self.runtime == 20):
+            self.nowst = random.randint(0, 3)
+        elif(self.runtime == 40):
+            self.nowst = self.nowst +4
+        elif (self.runtime == 60):
+            self.runtime = 0
+        self.state = self.nowst
+    def draw(self):
+        if self.state < 4:
+            self.image.clip_draw(self.frame * 64, (self.state * 64) - 1, 64, 64, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 0, ((self.state-4) * 64) - 1, 64, 64, self.x, self.y)
 
 class BackGround:
     def __init__(self):
@@ -198,8 +224,6 @@ class Object():
         self.image.draw(self.x,self.y)
 
 def collide(a, b):
-
-    global swt
     global bf
     left_a, bottom_a, right_a, top_a = a.get_bb()
     left_b, bottom_b, right_b, top_b = b.get_bb()
@@ -224,67 +248,56 @@ def collide(a, b):
 
     if stt in (a.RIGHT_RUN, a.RIGHT_STAND):
         if b.col == 0:
-            print("swt가 0이다!!!1")
             b.col = 1
             bf = 1
-            print(bf)
             return bf
         else:
-            print(bf)
             return bf
     elif stt in (a.DOWN_RUN, a.DOWN_STAND):
         if b.col == 0:
-            print("swt가 0이다!!!1")
             b.col = 1
             bf = 2
-            print(bf)
             return bf
         else:
-            print(bf)
             return bf
     elif stt in (a.LEFT_RUN, a.LEFT_STAND):
         if b.col == 0:
-            print("swt가 0이다!!!1")
             b.col = 1
             bf = 3
-            print(bf)
             return bf
         else:
-            print(bf)
             return bf
     elif stt in (a.UP_RUN, a.UP_STAND):
         if b.col == 0:
-            print("swt가 0이다!!!1")
             b.col = 1
             bf = 4
-            print(bf)
             return bf
         else:
-            print(bf)
             return bf
 
     return False
 
 def enter():
-    global swt
     global bf
     global human
+    global skeleton
     global background
     global box1, box2
     human = Human(50, 500)
+    skeleton = Skeleton(300, 500)
     background = BackGround()
     box1 = Object(0, 400,300 )
-    box2 = Object(0, 100, 200)
-    swt = 0
+    box2 = Object(0, 300, 200)
 def exit():
     global human
+    global skeleton
     global background
     global box1, box2
     del (human)
+    del (skeleton)
     del (background)
     del (box1)
     del (box2)
-
 def pause():
     pass
 
@@ -297,14 +310,16 @@ def handle_events():
     global human
     global box1, box2
     events = get_events()
+    skeleton.handle_event()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         else:
-            human.handle_event(event)
             pass
+        human.handle_event(event)
+
 
     if collide(human, box1):
         human.collideobject = collide(human, box1)
@@ -313,6 +328,7 @@ def handle_events():
 
 def update():
     human.update()
+    skeleton.update()
     delay(0.03)
 
 
@@ -322,10 +338,13 @@ def draw():
     box1.draw()
     box2.draw()
     human.draw()
+    skeleton.draw()
+
 
     human.draw_bb()
     box1.draw_bb()
     box2.draw_bb()
+    skeleton.draw()
 
     update_canvas()
 
